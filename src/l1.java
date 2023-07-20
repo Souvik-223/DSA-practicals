f0f0f4bddfba30d2ef7cf07350f652a924cf0209
Souvik chakraborty <22souvik22@gmail.com>
2023-07-20 11:48:24 +0530

    class 1

--- src/l1.java
+++ src/l1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class l1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner input = new Scanner(System.in);
+        //System.in refers to the keyboard input
+        System.out.println(input.nextInt());
+
+        //Primitive Datatype
+        int i = input.nextInt();
+        char letter = 'c';
+        float marks = input.nextFloat();  //f is added becuse by default any decimal value is double
+        double largedecimalnumbers = 4782974994.232939283;
+        long largeinteger = 237739739273937213L;  //L is added because by default any numeric value is int
+        boolean check = true;
+
+        //Non-Primitive Datatype
+        String s = input.nextLine();
+        int name[] = new int[10];
     }
 }
