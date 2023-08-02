dc92ed480d2d83322a91a12c3f2fd6471559c541
Souvik chakraborty <22souvik22@gmail.com>
2023-08-02 10:51:05 +0530

    Arrays and Variable length arguments

---
+++ src/Array_of_objects.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_of_objects {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String arr[] = new String[5];
+        // Values are given st runtime thus it only contains objects as each element is a primitive datatype
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=in.next();
+        }
+
+
+        // Multi dimension array - array of arrays
+        int marr[][] = new int[3][];  // only rows are mandatory not the columns because they could be varing.
+
+        // input in multi-dimension array
+        for(int i=0;i< marr.length;i++){
+            for(int j=0;j<marr[i].length;j++){
+                marr[i][j]=in.nextInt();
+            }
+        }
+
+        // output in multidimensional array
+        for (int i = 0; i < marr.length ; i++) {
+            System.out.println(Arrays.toString(marr[i]));  // every element in the array is an array itself that can be printed
+        }
+    }
+}
---
+++ src/Variable_length_arguments.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Variable_length_arguments {
+    public static void main(String[] args) {
+        fun(1,2,3,4,5,6,7,3,8,9);
+        fun(4,6,"souvik","kunal","Dhiraj","karan");
+        System.out.println();
+    }
+
+    // This is variable length arguments
+    static void fun(int...v){
+        System.out.println(v);
+    }
+
+    // Multiple values in variable length arguments
+    static void fun(int a, int b, String ...s){
+        System.out.println(a+b);
+        System.out.println(Arrays.toString(s));
+    }
+
+    // Function Overloading -> Same function in written again and again to provide flexibility to a function
+    static void hero(int age){}
+    static void hero(String name){}
+    static void hero(int age, String name){}
+}
